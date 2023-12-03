0d9899c30f61ae48b366f517bfd87f0b02395eca
LeviMaycon <levioficial361@gmail.com>
2023-12-03 13:57:39 -0300

    feat: added MongoDB env variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
+require('dotenv').config()
 
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://levi:92407786@cluster0.dcpjx.mongodb.net/?retryWrites=true&w=majority'
+      `mongodb+srv://levi:${process.env.CHAVE}@cluster0.dcpjx.mongodb.net/?retryWrites=true&w=majority`
     ),
     UsersModule
   ],
