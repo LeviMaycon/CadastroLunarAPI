902b8beaf6afec581f0fea2834748733b862a444
LeviMaycon <levioficial361@gmail.com>
2023-12-03 12:17:08 -0300

    feat(app): configured mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,9 @@ import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/nest'),
+    MongooseModule.forRoot(
+      'mongodb+srv://levi:@cluster0.dcpjx.mongodb.net/?retryWrites=true&w=majority'
+    ),
     UsersModule
   ],
   controllers: [],
